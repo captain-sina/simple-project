d45d2f9977abf7adae5e768ffe3e07ffa9486c3d
captain-sina <sinaborhaniit@gmail.com>
2020-11-02 19:12:57 +0330

    abort and redirect added

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask 
+from flask import Flask, request, make_response, redirect, abort
 app = Flask(__name__)
 
-@app.route('/')
-def hi():
-    ip = '198.192.1.1/255'
-    return "dello, sina"
+@app.route('/spidersite/user/<name>')
+def first(name):
+    if not name:
+        abort(404)
+    user_agent = request.headers.get("User_agent")
+    return f"<h2>{name}' profile</h2>\n<h6>browser{user_agent}"
 
-#some text
-#its ok 
-#don't worry yes
-#hahah
+@app.route('/spidersite')
+def main():
+    response = make_response("<h2>this page carries a cookie</h2>")
+    response.set_cookie('answer', '42')
+    return redirect('http://google.com/')
